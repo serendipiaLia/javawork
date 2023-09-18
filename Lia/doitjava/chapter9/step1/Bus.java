0e8b917189f856cb17b8491a1e126c27b613f888
serendipiaLia <cote9784@naver.com>
2023-09-18 18:18:47 +0900

    ch7 cartest

---
+++ Lia/doitjava/chapter9/step1/Bus.java
@@ -0,0 +1,26 @@
+package chapter9.step1;
+
+public class Bus extends Car{
+
+	@Override
+	public void run() { //Car클래스의 run과 refuel
+		System.out.println("버스가 달립니다.");
+	}
+
+	@Override //Car클래스의 run과 refuel
+	public void refuel() {
+		System.out.println("천연 가스를 충전합니다.");
+		
+	}
+
+	// 추상이 아님
+	public void takePassenger() {
+		System.out.println("승객을 버스에 태웁니다.");
+	} 
+
+	@Override
+	public void stop() {
+		System.out.println("차가 멈춥니다.");
+	}
+
+}
